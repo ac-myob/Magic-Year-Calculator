35c4f1f8c096e0df995120420f9f5389ff43a811
Andrew Christabel <andrew.christabel@myob.com>
2022-03-11 10:38:09 +1100

    Extracted person attributes into a person class

--- MagicYearCalculator.cs
+++ MagicYearCalculator.cs
@@ -4,21 +4,6 @@ using System.Text.RegularExpressions;
 
 namespace MagicYearCalculator // Note: actual namespace depends on the project name.
 {
-    class Person {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int AnnualSalary { get; set; }
-        public int StartYear { get; set; }
-
-        public string getFullName() {
-            return this.FirstName + " " + this.LastName;
-        }
-        public int getMonthlySalary() {
-            const int MONTHS_IN_YEAR = 12;
-            return (int)Math.Round((double)this.AnnualSalary/MONTHS_IN_YEAR);
-        }
-
-    }
     class MagicYearCalculator {
         
         private int getMagicYear(Person person, int magicConst=65) {
